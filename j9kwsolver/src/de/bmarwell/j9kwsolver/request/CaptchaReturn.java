68032c3264e33c05ac4bc0b9fd18c7cd7d051b76
Benjamin Marwell <bmarwell@gmail.com>
2013-12-17 09:22:33 +0100

    Added Copiright notice to each file

--- j9kwsolver/src/de/bmarwell/j9kwsolver/request/CaptchaReturn.java
+++ j9kwsolver/src/de/bmarwell/j9kwsolver/request/CaptchaReturn.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2010-2012, Benjamin Marwell.  This file is
+ * licensed under the Affero General Public License version 3 or later.  See
+ * the COPYRIGHT file.
+ */
 package de.bmarwell.j9kwsolver.request;
 
 public class CaptchaReturn {
